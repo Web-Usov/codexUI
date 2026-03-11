66da0cd2d4a8cfa5907c20d40a2f00948de53902
Igor <igor.levochkin@xrtc.io>
2026-03-11 07:45:38 +0700

    chore: bump version to 0.1.18 for npm publish

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import App from './App.vue'
 import router from './router'
 import './style.css'
 
-console.log('Welcome to codexapp. GitHub: https://github.com/friuns/codexui')
+console.log('Welcome to codexapp. GitHub: https://github.com/friuns2/codexui')
 
 createApp(App).use(router).mount('#app')
